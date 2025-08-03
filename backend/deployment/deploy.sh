179e925182481bcc55c1e7ff5a64cecd434710ee
qazalnaderi <ghazalnaderi4@gmail.com>
2025-08-03 21:36:46 +0330

    ready to deploy

--- backend/deployment/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Use multiple -f flags to include all service-specific and infrastructure Compose files
-docker compose -f docker-compose.yml \
-               -f ../infra/api-gateway/docker-compose.yml \
-               -f ../services/iam-service/docker-compose.yml \
-               -f ../services/media-service/docker-compose.yml \
-               -f ../services/core-service/docker-compose.yml \
-               up -d --build
